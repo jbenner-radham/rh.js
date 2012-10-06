6fef1a3758d6b07ba1c189e7173cbb158f5805f5
libra guest <james.benner@gmail.com>
2012-10-05 22:00:27 -0400

    Functional alpha push.

--- coffeescript/getJson.coffee
+++ coffeescript/getJson.coffee
@@ -1,46 +1,48 @@
-xmlRequest = ->
-  return new XMLHttpRequest()
+rhXhr =
+  xhr: ->
+    new XMLHttpRequest()
 
-xmlRequestParams = (page, callbackFunc, asynchronous, sendStr, httpMethod, responseType) ->
-  xmlParams =
-    page: page or null
-    callbackFunc: callbackFunc or null
-    asynchronous: asynchronous or true
-    sendStr: sendStr or null
-    httpMethod: httpMethod or 'GET'
-    responseType: responseType or null
-  return xmlParams
+  params:
+    page: null
+    callbackFunc: null
+    asynchronous: true
+    sendStr: null
+    httpMethod: 'GET'
+    responseType: null
 
-getXmlRequest = (page, callbackFunc, asynchronous, sendStr, httpMethod, responseType) ->
-  request = new xmlRequest()
-  params = xmlRequestParams page, callbackFunc, asynchronous, sendStr, httpMethod, responseType 
-  request.open params.httpMethod, params.page, params.asynchronous
-  request.send params.sendStr
-  request.onreadystatechange = ->
-    if request.readyState is 4 and request.status is 200
-      switch params.responseType
-        when 'json'
-          callbackFunc JSON.parse request.responseText
-        when 'text', null
-          callbackFunc request.responseText
-          
-getJson = (page, callbackFunc, asynchronous, sendStr, httpMethod) ->
-  getXmlRequest page, callbackFunc, asynchronous, sendStr, httpMethod, 'json'
+  xhrRequest: (page, callbackFunc, asynchronous, sendStr, httpMethod, responseType) ->
+    request = new @xhr()
+    p = @params
+    request.open @params.httpMethod, @params.page, @params.asynchronous
+    request.send @params.sendStr
+    request.onreadystatechange = ->
+      # @/this takes on a different value here since it's inside the onreadystatechange method.
+      if request.readyState is 4 and request.status is 200
+        switch p.responseType
+          when "json"
+            p.callbackFunc JSON.parse(request.responseText)
+          when "text", null
+            p.callbackFunc request.responseText
 
-getText = (page, callbackFunc, asynchronous, sendStr, httpMethod) ->  
-  getXmlRequest page, callbackFunc, asynchronous, sendStr, httpMethod, 'text'
+  getText: (page, callbackFunc, asynchronous, httpMethod) ->
+    p = @params
+    p.page = page or null
+    p.callbackFunc = callbackFunc or null
+    p.responseType = 'text'
+    @xhrRequest()
 
-callbackResponse = (requestReturn) ->
-  switch typeof requestReturn
-    when 'object'
-      for x of requestReturn
-        console.log requestReturn[x]
-    when 'string'
-      console.log requestReturn
-      
+  getJson: (page, callbackFunc, asynchronous, httpMethod) ->
+    p = @params
+    p.page = page or null
+    p.callbackFunc = callbackFunc or null
+    p.responseType = 'json'
+    @xhrRequest()
+
+###
 domPurgeChildren = (domElement) ->
   while domElement.childNodes.length >= 1
     domElement.removeChild domElement.firstChild
+###
 
 # Test functions...
 #getJson 'echo.php', callbackResponse
--- javascript/getJson.js
+++ javascript/getJson.js
@@ -1,86 +1,61 @@
-var rhXhr = {
-    
-    xhr: function() {
-		return new XMLHttpRequest();
-	},
-    
-    params: {
-        page: null,
-        callbackFunc: null,
-        asynchronous: true,
-        sendStr: null,
-        httpMethod: 'GET',
-        responseType: null
-    },
-	
-    /* xhrParams is depricated, will be removed. */
-	xhrParams: function(page, callbackFunc, asynchronous, sendStr, httpMethod, responseType) {
-		/*
-		var _method = this.xhrParams;
-		_method.page = page || null;
-		_method.callbackFunc = callbackFunc || null;
-		_method.asynchronous = asynchronous || true;
-		_method.sendStr = sendStr || null;
-		_method.httpMethod = httpMethod || 'GET';
-		_method.responseType = responseType || null;
-		*/
-		var paramsObj = {
-			page: page || null,
-            callbackFunc: callbackFunc || null,
-            asynchronous: asynchronous || true,
-            sendStr: sendStr || null,
-            httpMethod: httpMethod || 'GET',
-            responseType: responseType || null
-		};
-		return paramsObj;
-	},
-	
-	xhrRequest: function(page, callbackFunc, asynchronous, sendStr, httpMethod, responseType) {
-		var request = new this.xhr();
-		var params = this.params;
-        request.open(params.httpMethod, params.page, params.asynchronous);
-        request.send(params.sendStr);
-        return request.onreadystatechange = function() {
-            if (request.readyState === 4 && request.status === 200) {
-                switch (params.responseType) {
-                    case 'json':
-                        return params.callbackFunc(JSON.parse(request.responseText));
-                    case 'text':
-                    case null:
-                        return params.callbackFunc(request.responseText);
-                }
-            }
-        };
-    },
+var rhXhr;
 
-	test2: function() {
-		console.log('test2');
-	},
-
-	testParams: function() {
-		console.log(this.xhrParams.page);
-	},
-	
-	getText: function(page, callbackFunc, asynchronous, sendStr, httpMethod) {
-		this.params.page = page || null;
-		this.params.callbackFunc = callbackFunc || null;
-		this.params.responseType = 'text';
-		this.xhrRequest();
-	},
-	
-	getJson: function(page, callbackFunc, asynchronous, sendStr, httpMethod) {
-		this.xhrRequest(page, callbackFunc, asynchronous, sendStr, httpMethod, 'json');
-	}
-
+rhXhr = {
+  xhr: function() {
+    return new XMLHttpRequest();
+  },
+  params: {
+    page: null,
+    callbackFunc: null,
+    asynchronous: true,
+    sendStr: null,
+    httpMethod: 'GET',
+    responseType: null
+  },
+  xhrRequest: function(page, callbackFunc, asynchronous, sendStr, httpMethod, responseType) {
+    var p, request;
+    request = new this.xhr();
+    p = this.params;
+    request.open(this.params.httpMethod, this.params.page, this.params.asynchronous);
+    request.send(this.params.sendStr);
+    return request.onreadystatechange = function() {
+      if (request.readyState === 4 && request.status === 200) {
+        switch (p.responseType) {
+          case "json":
+            return p.callbackFunc(JSON.parse(request.responseText));
+          case "text":
+          case null:
+            return p.callbackFunc(request.responseText);
+        }
+      }
+    };
+  },
+  getText: function(page, callbackFunc, asynchronous, httpMethod) {
+    var p;
+    p = this.params;
+    p.page = page || null;
+    p.callbackFunc = callbackFunc || null;
+    p.responseType = 'text';
+    return this.xhrRequest();
+  },
+  getJson: function(page, callbackFunc, asynchronous, httpMethod) {
+    var p;
+    p = this.params;
+    p.page = page || null;
+    p.callbackFunc = callbackFunc || null;
+    p.responseType = 'json';
+    return this.xhrRequest();
+  }
 };
 
+/*
 var cbJson = function(returnJson) {
 	console.log(returnJson);
 	//for (var x in returnJson) {
 	//	console.log(returnJson[x]);
 	//}
 };
-
+*/
 /*
 for (var key in rhXhr) {
   // if the keys belongs to object and it is a function
@@ -92,4 +67,4 @@ for (var key in rhXhr) {
 //rhXhr.xhrParams();
 //rhXhr.testParams();
 
-rhXhr.getText('echo.php', cbJson);
+//rhXhr.getText('echo.php', cbJson);
