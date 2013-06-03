4fbd7bc915b7c5f50958e803b8fd8be0f1580c32
OpenShift guest <james.benner@gmail.com>
2013-06-02 21:51:27 -0400

    Added several new psuedo-methods (character.upper.is, lower.is, special.is & word.spaceCaps).

--- javascript/RH.js
+++ javascript/RH.js
@@ -24,12 +24,23 @@ var RH = (function() {
 
                         return selector.slice(0, ++i);
 
+                    },
+
+                    left: function() {
+
+                        var i, len = selector.length - 1;
+
+                        for (i = 0; selector[i] === ' '; ++i);
+
+                        return selector.slice(i);
+
                     }
 
                 };
                 
                 // aliases for trim{}...
                 this.trimR = function() { return this.trim.right() };
+                this.trimL = function() { return this.trim.left() };
 
                 this.word = {
 
@@ -43,12 +54,85 @@ var RH = (function() {
                         // The accompanying ternary statement for the no space scenario.
                         return str.slice(i? ++i : 0);
 
+                    },
+
+                    spaceCaps: function() {
+
+                        var str = _methodSelf.trim.left(), rtrnStr = '';
+
+                        for (var i = 0, len = str.length; i < len; ++i) {
+
+                            rtrnStr += (_self(str[i]).isUC() && i !== 0? ' ' : '') + str[i];
+
+                        }
+
+                        return rtrnStr;
+
                     }
 
                 };
-                
+
                 // aliases for word{}...
                 this.wordLast = function() { return this.word.last() };
+                this.automagicSpaces = function() { return this.word.spaceCaps() }
+
+                this.character = {
+
+                    init: function() {
+
+                        // _methodSelf.character.special.is()) << This doesn't work because it goes on a recursive loop, duh me!
+                        if (!isNaN(selector) || '~`!#$%^&*+=-[]\\\';,/{}|":<>?'.indexOf(selector) !== -1)
+                            return false;
+
+                        return selector.length === 1? selector : selector[0];
+
+                    },
+
+                    upper: {
+
+                        is: function() {
+
+                            var c = _methodSelf.character.init();
+
+                            return c && c === c.toUpperCase();
+
+                        }
+
+                    },
+
+                    lower: {
+
+                        is: function() {
+
+                            var c = _methodSelf.character.init();
+
+                            return c && c === c.toLowerCase();
+
+                        }
+
+                    },
+
+                    special: {
+
+                        is: function() {
+                            
+                            var c = _methodSelf.character.init();
+
+                            if (!c) return false;
+                            
+                            return '~`!#$%^&*+=-[]\\\';,/{}|":<>?'.indexOf(c) !== -1;
+
+                        }
+
+                    }
+
+                };
+
+                this.isUC = function() { return this.character.upper.is() }
+
+                this.isLC = function() { return this.character.lower.is() }
+
+                this.isSpc = function() { return this.character.special.is() }
 
             }
 
