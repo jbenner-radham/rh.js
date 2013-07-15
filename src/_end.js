7c91a3b9d53d59d61012c053753fab943ab5746b
James Benner <james.benner@gmail.com>
2013-07-14 23:22:07 -0400

    Updated with fixed "RH.getJson" method.

--- src/_end.js
+++ src/_end.js
@@ -1,3 +1,17 @@
 
   return _self;
 })();
+
+RH.getJson = function(jsonUri, cbFn) {
+
+    var request = new XMLHttpRequest();
+
+    request.overrideMimeType('application/json');
+    request.open('GET', jsonUri, true);
+    request.send(null);
+    
+    request.onreadystatechange = function() {
+        if (request.readyState === 4 && request.status === 200)
+            return cbFn(JSON.parse(request.responseText));
+    };
+};
