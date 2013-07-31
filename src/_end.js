551a02ffc8d9bf78b6faa8ef33178daeeaa3fa24
James Benner <james.benner@gmail.com>
2013-07-31 13:59:19 -0400

    Added the ".getQueryString()" function.

--- src/_end.js
+++ src/_end.js
@@ -1,17 +1,35 @@
 
-  return _self;
+    return _self;
 })();
 
-RH.getJson = function(jsonUri, cbFn) {
+RH.getJson = function(uri, cbFn) {
 
-    var request = new XMLHttpRequest();
+    var request = new XMLHttpRequest()
 
-    request.overrideMimeType('application/json');
-    request.open('GET', jsonUri, true);
-    request.send(null);
+    request.overrideMimeType('application/json')
+    request.open('GET', uri, true)
+    request.send(null)
     
     request.onreadystatechange = function() {
         if (request.readyState === 4 && request.status === 200)
-            return cbFn(JSON.parse(request.responseText));
-    };
-};
+            return cbFn(JSON.parse(request.responseText))
+    }
+}
+
+RH.getQueryString = function() {
+
+    var query = decodeURIComponent(document.location.search),
+        qsObj = {}
+
+    // Checking if "query" is an empty string is technically
+    // un-needed since the "query[0]" check appears to handle
+    // it fine. However I put it in there because I'm paranoid.
+    if (query === '' || query[0] !== '?') return null
+
+    query.slice(1).split('&').forEach(function(argv) {
+        var getVar = argv.split('=')
+        qsObj[getVar[0]] = getVar[1]
+    })
+
+    return qsObj
+}
