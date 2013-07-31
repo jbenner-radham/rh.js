beac5001a25b7517c91b483964dad81bf294f0ea
jbenner55 <james.benner@gmail.com>
2013-07-31 00:02:12 -0400

    Create scratchpad.js

---
+++ javascript/scratchpad.js
@@ -0,0 +1,15 @@
+function getQueryStringObj() {
+
+  var query = document.location.search,
+      qsObj = {}
+
+  if (query[0] !== '?') return false
+
+  query.slice(1).split('&').forEach(function(argv) {
+    var getVar = argv.split('=')
+        qsObj[getVar[0]] = getVar[1]
+  })
+
+  return qsObj
+  
+}
